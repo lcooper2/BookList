a7ac9305a7a9161b74b610a64c7b087794b68f8e
Logan <logantcooper6@gmail.com>
2019-09-03 23:56:11 -0600

    Booklist in class work

---
+++ BookList/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore; // Connect to data sources
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        DbSet<Book> Book { get; set; }
+    }
+}
---
+++ BookList/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Models
+{
+    public class Book
+    {
+        // Data annotation attributes
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Book Name")]
+        [Required]
+        public string Name { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+    }
+}
